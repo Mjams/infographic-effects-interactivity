45f47eb2cc7d4819c74a0a612ffe7874ba598969
Mjams <maggie.paquette@hotmail.ca>
2016-04-22 08:59:18 -0400

    added waypoints

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var $gasJug = $('#jug');
+var $txt = $('.txt');
+
+$('.gas-jug').on('mouseover', function () {
+  $gasJug.addClass('jug-turn');
+});
+
+$('.gas-jug').on('mouseout', function () {
+  $gasJug.removeClass('jug-turn');
+});
+
+$txt.waypoint(function (direction) {
+  if (direction == 'down') {
+    $txt.addClass('js-txt-animate');
+  } else {
+    $txt.removeClass('js-txt-animate');
+  }
+}, { offset: '70%' });
